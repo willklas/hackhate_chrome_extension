871353591963b18c45810756a08ad42bca0d2c33
unknown <william_klassen@hotmail.com>
2021-10-14 19:55:59 -0400

    updates

--- content.js
+++ content.js
@@ -92,45 +92,163 @@ function isElementInViewport(el) {
     
 }
 
+
+// document.addEventListener('DOMContentLoaded', function() {
+//     var theButton = document.getElementById('poop1');
+//     theButton.addEventListener('click', function() {
+//         alert('damn');
+//     });
+// });
+
+
+
+
 const elements_seen      = new Set()
 const elements_processed = new Set()
+const collection = new Map();
+var button_id = 0
+
+function unblur_image(event) {
+    event.stopImmediatePropagation();
+    event.stopPropagation()
+    event.preventDefault();
+
+    console.log("hahaha", event.target.id)
+    var theButton = document.getElementById(event.target.id)
+    console.log(theButton.previousSibling.nodeName)
+    collection.get(event.target.id).style.webkitFilter = "blur(0px)";
+    node = collection.get(event.target.id)
+    theButton.parentNode.removeChild(theButton) // remove <a>
+
+    return
+}
+
+
 function do_stuff() {
     window.requestAnimationFrame(function() {
         // var arrOfPtags = document.getElementsByTagName("p");
         var arrOfPtags = document.querySelectorAll('img')
+        console.log("here 1");
         for (var i = 0;i < arrOfPtags.length; i++){
+            console.log("here 2");
             // arrOfPtags[i].setAttribute("desired_attribute", "value");
             if (!elements_seen.has(arrOfPtags[i])) {
                 var el_view = isElementInViewport(arrOfPtags[i]);
-                if (el_view > .2) {
+                if (el_view > .4) {
+                    elements_seen.add(arrOfPtags[i]);
                     console.log('Element is sig. in the viewport!', el_view);
                     arrOfPtags[i].style.webkitFilter = "blur(6px)";
                     elements_processed.add(arrOfPtags[i])
 
+                    var button = document.createElement("button");
+                    button.innerHTML = "show content?";
+                    
+                    
+                    // button.onclick = function() {
+                    //     console.log("clicked");
+                    // }
+                    button.id = "poop" + button_id.toString()
+                    button.onclick="event.stopPropagation();"
+                    button_id += 1
 
+                    // var theButton = document.getElementById('poop1');
+                    // theButton.addEventListener('click', function() {
+                    //     alert('damnfffffffffff');
+                    // });
+
+                    var image_node   = arrOfPtags[i]
+                    
+                    var super_parent = arrOfPtags[i].parentNode.parentNode
+                    var button_node  = button
+
+                    var wrapper = document.createElement("div");
+                    wrapper.id = "hehehaha"
+
+                    // arrOfPtags[i].append(button);
+                    // console.log("...............",arrOfPtags[i].parentNode.nodeName)
+                    // if (arrOfPtags[i].parentNode.nodeName == "A") {
+                    //     super_parent.insertBefore(wrapper, parent_node)
+                    //     super_parent.removeChild(parent_node) // remove <a>
+                    //     wrapper.insertBefore(parent_node, null)
+                    //     arrOfPtags[i].parentNode.parentNode.insertBefore(button, arrOfPtags[i].parentNode.nextSibling);
+
+                    // }
+                    // else {
+                    //     arrOfPtags[i].parentNode.insertBefore(button, arrOfPtags[i].nextSibling);
+                        
+                    // }
+
+
+                    var parent_node = arrOfPtags[i].parentNode
+                    parent_node.insertBefore(wrapper, arrOfPtags[i])
+                    parent_node.removeChild(arrOfPtags[i])
+                    wrapper.insertBefore(arrOfPtags[i], null)
+                    wrapper.insertBefore(button, null)
+                    // parent_node.insertBefore(button, arrOfPtags[i].nextSibling);
+
+                    // arrOfPtags[i].parentNode.insertBefore(button, arrOfPtags[i].nextSibling);
+                    
+                    
+
+                    button.addEventListener('click', unblur_image);
+                    collection.set(button.id, arrOfPtags[i]);
+
+                    // wrapper.style = "position:relative;width: 50%;"
+
+
+
+
+                    // button.style = "margin: 0; \
+                    //                 position: absolute; \
+                    //                 top: 50%; \
+                    //                 left: 50%; \
+                    //                 -ms-transform: translate(-50%, -50%); \
+                    //                 transform: translate(-50%, -50%);"
+                    
+                    test        = "position: absolute; \
+                                   top: 50%; \
+                                   left: 50%; \
+                                   transform: translate(-50%, -50%); \
+                                   -ms-transform: translate(-50%, -50%); \
+                                   background-color: #555; \
+                                   color: white; \
+                                   font-size: 12px; \
+                                   padding: 12px 24px; \
+                                   border: none; \
+                                   cursor: pointer; \
+                                   border-radius: 5px;"
+                    test = "position: absolute;top: 50%;left: 50%;transform: translate(-50%, -50%);-ms-transform: translate(-50%, -50%);"
+                    wrapper.style="display:inline-block;position:relative;"
+                    button.style = test
+
+
+                    
+
+                    
+
+                    // var theButton = document.getElementById('poop1');
+                    // button.addEventListener('click', test);
+                    
 
 
                     // org_html = document.getElementById("slidesContainer").innerHTML;
-                    org_html = arrOfPtags[i].innerHTML;
-                    new_html = "<div class='container'>" + org_html + "<button class='btn'>Button</button></div>";
                     // document.getElementById("slidesContainer").innerHTML = new_html;
-                    arrOfPtags[i].innerHTML = new_html;
+
+                    
+                    // org_html = arrOfPtags[i].innerHTML;
+                    // new_html = "<div class='container'>" + org_html + "<button class='btn'>Button</button></div>";
+                    // arrOfPtags[i].innerHTML = new_html;
 
                     // <div class="container">
                     // <img src="img_snow.jpg" alt="Snow">
                     // <button class="btn">Button</button>
                     // </div>
-
-
-
-
-
+                   
                 }
-                else {
-                    arrOfPtags[i].style.webkitFilter = "blur(0px)";
-                }
+                // else {
+                //     arrOfPtags[i].style.webkitFilter = "blur(0px)";
+                // }
             }
-            elements_seen.add(arrOfPtags[i])
         }
     });
     return;
@@ -138,6 +256,7 @@ function do_stuff() {
 
 document.addEventListener('scroll', function(e) {
     // lastKnownScrollPosition = window.scrollY;
+    // console.log('Scroll event listener hit!');
     do_stuff()
     return;
 });
